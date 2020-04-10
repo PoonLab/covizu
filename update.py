cff7126d2da31663f5cbc04600b6a5718ec522d5
Art Poon <artpoon@gmail.com>
2020-04-10 12:41:42 -0400

    added updater script

---
+++ update.py
@@ -0,0 +1,31 @@
+import argparse
+from gotoh2 import *
+
+parser = argparse.ArgumentParser(
+    description="Update previous FASTA alignment with new sequences by"
+                "pairwise alignment to a reference genome."
+)
+
+parser.add_argument('srcfile', type=argparse.FileType('r'),
+                    help='input, FASTA file with new sequences')
+parser.add_argument('destfile', type=argparse.FileType('r+'),
+                    help='input/output, FASTA file to append to')
+
+parser.add_argument('-ref', default=open('data/NC_045512.fa'),
+                    type=argparse.FileType('r'),
+                    help='Path to reference sequence.')
+
+args = parser.parse_args()
+
+
+def progress(msg):
+    print(msg)
+
+
+# load SARS-COV-2 reference genome
+refseq = read_seq(args.ref)
+aligner = Aligner()
+update = update_alignment(
+    ref=refseq, src=args.srcfile, dest=args.destfile, aligner=aligner,
+    callback=progress
+)
