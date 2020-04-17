51737b73a8ff4c34d5c63b0ea7bbbc24e2d3f65d
ArtPoon <artpoon@gmail.com>
2020-04-17 17:05:13 -0400

    got it working!

--- draw-mst.R
+++ draw-mst.R
@@ -2,16 +2,19 @@ setwd('~/git/covizu/')
 
 # load cluster info
 clusters <- read.csv('data/clusters.info.csv', header=T)
-names(clusters) <- c('label', 'parent', 'coldate', 'region', 'country')
+names(clusters) <- c('label', 'node.name', 'coldate', 'region', 'country')
 
 clusters$coldate <- as.Date(clusters$coldate)
 clusters$accession <- sapply(clusters$label, function(x) {
   strsplit(as.character(x), "\\|")[[1]][2]
 })
+clusters$desc <- sapply(clusters$label, function(x) {
+  strsplit(as.character(x), "\\|")[[1]][1]
+})
 
 # load first component of split MST
 files <- Sys.glob('mst/component-*.edgelist.csv')
-f <- files[1]
+f <- files[3]
 
 
 edges <- read.csv(f, header=T)
@@ -54,23 +57,38 @@ res <- traverse(root, edges)
 nodes <- nodes[match(res, nodes$accession), ]
 nodes$y <- 1:nrow(nodes)
 
+x <- c(nodes$mindate, nodes$maxdate)
 
-par(mar=c(2,0,0,2))
-plot(NA, xlim=c(min(nodes$mindate), max(nodes$maxdate)), 
-     ylim=c(1, nrow(nodes)), bty='n', xaxt='n')
-for (i in 1:nrow(nodes)) {
-  n <- nodes[i,]
-  segments(x0=n$mindate, x1=n$maxdate, y0=n$y)
-  
-  # draw bubbles
-  
-  
-  text(max(nodes$maxdate), n$y, label=n$accession, cex=0.5, adj=0)
-}
+par(mar=c(2,0,0,6)+0.1)
+plot(NA, xlim=range(x), ylim=c(1, nrow(nodes)), 
+     bty='n', xaxt='n', yaxt='n')
+axis(side=1, at=pretty(x), label=strftime(pretty(x), format='%b %d'), 
+     cex.axis=0.8, mgp=c(4,0.5,0))
+
 for (j in 1:nrow(edges)) {
   e <- edges[j, ]
   n1 <- nodes[as.character(nodes$accession)==as.character(e$parent), ]
   n2 <- nodes[as.character(nodes$accession)==as.character(e$child), ]
   arrows(x0=e$coldate, y0=n1$y, y1=n2$y, length=0.05, col='grey')  
+}
+
+for (i in 1:nrow(nodes)) {
+  n <- nodes[i,]
+  segments(x0=n$mindate, x1=n$maxdate, y0=n$y, col='cadetblue')
+  
+  # draw bubbles
+  cluster <- clusters[clusters$accession==n$accession,]
+  temp <- table(cluster$coldate)
+  reg <- split(cluster$region, cluster$coldate)
+  
+  points(x=as.Date(names(temp)), y=rep(n$y, length(temp)), 
+         cex=sqrt(temp), pch=21, 
+         bg=sapply(reg, function(x) ifelse('Canada' %in% x, 'red', 'white'))
+         )
+  if (n$count > 5) {
+    text(max(nodes$maxdate)+0.5, n$y, 
+         label=clusters$desc[clusters$accession==n$accession][1], 
+         cex=0.5, adj=0)
+  }
 }
 
