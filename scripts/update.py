ab79293b6ddae9d3ab36703681cece8857abbb37
Art Poon <artpoon@gmail.com>
2020-06-04 13:52:07 -0400

    refactored update.py

--- scripts/update.py
+++ scripts/update.py
@@ -1,32 +1,51 @@
 import argparse
 from gotoh2 import *
 
-parser = argparse.ArgumentParser(
-    description="Update previous FASTA alignment with new sequences by"
-                "pairwise alignment to a reference genome."
-)
 
-parser.add_argument('srcfile', type=argparse.FileType('r'),
-                    help='input, FASTA file with new sequences')
-parser.add_argument('destfile', type=argparse.FileType('r+'),
-                    help='input/output, FASTA file to append to')
+def update(srcfile, destfile, reffile, callback=None):
+    """
+    Incremental update of destination FASTA file by pairwise alignment
+    of sequences from source FASTA file against a common reference.
 
-parser.add_argument('-ref', default=open('data/NC_045512.fa'),
-                    type=argparse.FileType('r'),
-                    help='Path to reference sequence.')
+    :param srcfile: open file stream to source FASTA in read mode
+    :param destfile: open file stream to destination FASTA in append (r+) mode
+    :param reffile: open file stream to FASTA with reference genome
+    :param callback: optional, callback function for progress monitoring
+    :return: int, number of genome sequences appended to destfile
+    """
+    # load SARS-COV-2 reference genome
+    _, refseq = convert_fasta(reffile)[0]
 
-args = parser.parse_args()
+    aligner = Aligner()
+    count = update_alignment(
+        ref=refseq, src=srcfile, dest=destfile, aligner=aligner,
+        callback=callback
+    )
+    return count
 
 
-def progress(msg):
-    print(msg)
+def parse_args():
+    """ Command-line interface """
+    parser = argparse.ArgumentParser(
+        description="Update previous FASTA alignment with new sequences by"
+                    "pairwise alignment to a reference genome."
+    )
+    parser.add_argument('srcfile', type=argparse.FileType('r'),
+                        help='input, FASTA file with new sequences')
+    parser.add_argument('destfile', type=argparse.FileType('r+'),
+                        help='input/output, FASTA file to append to')
+    parser.add_argument('-ref', default=open('data/NC_045512.fa'),
+                        type=argparse.FileType('r'),
+                        help='Path to reference sequence.')
+
+    return parser.parse_args()
 
 
-# load SARS-COV-2 reference genome
-_, refseq = convert_fasta(args.ref)[0]
+if __name__ == '__main__':
+    args = parse_args()
 
-aligner = Aligner()
-update = update_alignment(
-    ref=refseq, src=args.srcfile, dest=args.destfile, aligner=aligner,
-    callback=progress
-)
+    def progress(msg):
+        print(msg)
+
+    update(srcfile=args.srcfile, destfile=args.destfile, reffile=args.ref,
+           callback=progress)
