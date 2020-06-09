7e88b9015979abbdb42b9ba173adace542eaa552
emmanuel <ewong347@uwo.ca>
2020-06-08 23:54:48 -0400

    one more before i sleep

--- scripts/ChunkyBot.py
+++ scripts/ChunkyBot.py
@@ -21,15 +21,16 @@ cwd = os.getcwd()
 def compare_dicts(old_fasta, new_fasta):
 	old_dict = dict(convert_fasta(open(old_fasta)))
 	new_dict = dict(convert_fasta(open(new_fasta)))
-	block_diff={}
+	val_diff={}
+	miss_diff={}
 	for key, value in old_dict.items():
 		try:
 			newval = new_dict[key]
 			if newval != value:
-				block_diff[key] = newval
+				val_diff[key] = newval
 		except:
-			block_diff[key] = value
-	return block_diff
+			miss_diff[key] = value
+	return miss_diff, val_diff
 
 
 
@@ -52,7 +53,11 @@ def parse_args():
 		)
 	parser.add_argument(
 		'-o', '--outputdiffFasta', type=str, default='data/weeklydumps/diff.fasta',
-		help='Directory for Output file containing sequences that are modified or missing'
+		help='Directory for Output file containing sequences that are modified'
+		)
+	parser.add_argument(
+		'-o2', '--outputmissFasta', type=str, default='data/weeklydumps/miss.fasta',
+		help='Directory for Output file containing sequences that are missing'
 		)
 	return parser.parse_args()
 
@@ -63,21 +68,28 @@ if __name__ == '__main__':
 	driver = get_driver(download_folder=download_folder, executable_path=args.binpath)
 	driver = login(driver=driver)
 	bfiles = ['data/baseline/'+ s for s in os.listdir(args.baselinedir)]
-	difffasta ={}
+	difffasta = {}
+	missfasta = {}
 	#predefined blocks one, two and three have <10k sequences
 	srcfile = retrieve_genomes(driver=driver, start='2019-01-01', end='2020-04-16',
 							   download_folder=download_folder)
-	difffasta.update(compare_dicts(bfiles[0],srcfile))
+	miss_dict, val_dict = compare_dicts(bfiles[0],srcfile)
+	difffasta.update(val_dict)
+	missfasta.update(miss_dict)
 	time.sleep(300)
 
 	srcfile = retrieve_genomes(driver=driver, start='2020-04-17', end='2020-05-08',
 							   download_folder=download_folder)
-	difffasta.update(compare_dicts(bfiles[1],srcfile))
+	miss_dict, val_dict = compare_dicts(bfiles[1],srcfile)
+	difffasta.update(val_dict)
+	missfasta.update(miss_dict)
 	time.sleep(300)
 
 	srcfile = retrieve_genomes(driver=driver, start='2020-05-08', end='2020-05-15',
 							   download_folder=download_folder)
-	difffasta.update(compare_dicts(bfiles[2],srcfile))
+	miss_dict, val_dict = compare_dicts(bfiles[2],srcfile)
+	difffasta.update(val_dict)
+	missfasta.update(miss_dict)
 	time.sleep(300)
 
 	#next do the chunks afterwards in weekly increments
@@ -101,17 +113,23 @@ if __name__ == '__main__':
 
 		srcfile = retrieve_genomes(driver=driver, start=datetime.strftime(startdate, '%Y-%m-%d'),
 								   end=datetime.strftime(enddate, '%Y-%m-%d'), download_folder=download_folder)
-		difffasta.update(compare_dicts(bfiles[osdircount],srcfile))
+		miss_dict, val_dict = compare_dicts(bfiles[osdircount],srcfile)
+		difffasta.update(val_dict)
+		missfasta.update(miss_dict)
 		osdircount +=1
 		time.sleep(300)
 		startdate = enddate +timedelta(1)
 		#set up for next iteration
 
 	#move latest file directly to baseline
-	shutil.move(srcfile, cwd+'/data/baseline/'+blockname+ '.fasta')
+	shutil.move(srcfile, cwd+'/data/baseline/GISAID-'+blockname+ '.fasta')
 
 	driver.quit()
 
 	with open(args.outputdiffFasta, mode='w+') as out:
 		for h,s in difffasta.items():
+			out.write('>{}\n{}\n'.format(h, s))
+
+	with open(args.outputmissFasta, mode='w+') as out2:
+		for h,s in missfasta.items():
 			out.write('>{}\n{}\n'.format(h, s))
