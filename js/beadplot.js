05c0c18854757bf44b92f44b8eebcadf5c0ff50e
kwade4 <kwade4@uwo.ca>
2020-06-11 16:28:35 -0400

    Implemented bar chart for region distribution (#25, #67)

--- js/beadplot.js
+++ js/beadplot.js
@@ -332,6 +332,7 @@ function beadplot(cid) {
         // Display region distribution in tooltip
         let my_regions = table(d.region),
             tooltipText = `<b>Number of cases</b><br>`;
+
         for (let [r_key, r_value] of Object.entries(my_regions)) {
           tooltipText += `${r_key}: ${r_value}<br>`
         }
@@ -365,8 +366,8 @@ function beadplot(cid) {
 	console.log(mystr)
         $("#text-node").html(mystr);
 	console.log(mystr)
-        // TODO: render as a barchart?
-        console.log(table(d.region));
+
+        draw_region_distribution(table(d.region));
       });
 
   // draw x-axis
@@ -393,3 +394,95 @@ function gentable(my_countries){
 }
 
 
+/**
+ * Draws a bar chart of the distribution of cases across regions
+ * @param {{}} my_regions: associative list of region and case count pairs
+ */
+function draw_region_distribution(my_regions) {
+
+    // Include all regions
+    const regions = ["Africa", "Asia", "China", "Europe", "North America", "Oceania", "South America"];
+    regions.forEach(function(reg) {
+        if (!my_regions.hasOwnProperty(reg)) {my_regions[reg] = 0;}
+    });
+
+    // Store {region, count} pairs in a list, sorted in decreasing order
+    const regKeys = Object.keys(my_regions),
+        regionOrder = regKeys.sort(function(a, b) {return my_regions[b] - my_regions[a]}),
+        sortedRegions = [];
+    regionOrder.forEach(function(reg, idx) {
+      sortedRegions.push({"region":regionOrder[idx], "count": my_regions[reg]})
+    });
+
+    // Set the margins
+    const margin = 60,
+        width = 600 - 2 * margin,
+        height = 400 - 2 * margin;
+
+    // Create the barchart
+    const svg = d3.select("#text-node")
+        .append("svg")
+        .attr("width", 600)
+        .attr("height", 400);
+    const chart = svg.append("g")
+        .attr("transform", `translate(${margin}, ${margin})`);
+
+    // Set the scale of the x-axis
+    const xScale = d3.scaleBand()
+        .range([0, width])
+        .domain(sortedRegions.map((r) => r.region))
+        .padding(0.2);
+    chart.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(xScale));
+
+    // Set the scale of the y-axis
+    const yScale = d3.scaleLinear()
+        .range([height, 0])
+        .domain([0, sortedRegions[0].count])
+        .nice()
+    chart.append("g")
+        .call(d3.axisLeft(yScale).ticks((sortedRegions[0].count < 10) ? sortedRegions[0].count : 10));
+
+    const regionBars = chart.selectAll()
+        .data(sortedRegions)
+        .enter()
+        .append("g")
+
+    // Draw bars on the bar chart
+    regionBars
+        .append("rect")
+        .attr("x", (r) => xScale(r.region))
+        .attr("y", (r) => yScale(r.count))
+        .attr("height", (r) => height - yScale(r.count))
+        .attr("width", xScale.bandwidth())
+        .attr("fill", "darkgray");
+
+    // Write the case count above each bar
+    regionBars.append("text")
+        .style("font", "0.8em/1.2 Lato, sans-serif")
+        .attr("x", (r) => xScale(r.region) + xScale.bandwidth() / 2)
+        .attr("y", (r) => yScale(r.count) - 10)
+        .attr("text-anchor", "middle")
+        .text((r) => `${r.count}`);
+
+    // Add axis labels
+    svg.append("text")
+        .attr("x", width / 2 + margin)
+        .attr("y", 40)
+        .attr("text-anchor", "middle")
+        .text("Cases by Region");
+
+    svg.append("text")
+        .attr("x", -(height / 2) - margin)
+        .attr("y", margin/ 3)
+        .attr("transform", "rotate(-90)")
+        .attr("text-anchor", "middle")
+        .text("Number of Cases");
+
+    svg.append("text")
+        .attr("x", (width / 2) + margin)
+        .attr("y", height + margin * 1.8)
+        .attr("text-anchor", "middle")
+        .text("Region")
+}
