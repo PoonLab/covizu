1449f5483edb311d7cad1a379e0ad621e67a7cd7
emmanuel <ewong347@uwo.ca>
2020-07-27 10:17:20 -0400

    Changing how backup files are removed

--- backup-update.sh
+++ backup-update.sh
@@ -3,13 +3,12 @@
 #Copy database
 cp data/gsaid.db "data/backup/$(date +"%Y_%m_%d").gsaid.db"
 
-echo "data/backup/$(date +"%Y_%m_%d").gsaid.db"
-
 #Run updater script
-python3 scripts/Chunkybot.py --db "data/backup/$(date +"%Y_%m_%d").gsaid.db" >> debug/Chunkybot.log
+python3 scripts/ChunkyBot.py -db "data/backup/$(date +"%Y_%m_%d").gsaid.db" >> debug/Chunkybot.log
 
 #Migrate missing entries
-python3 scripts/db_utils.py --targetdb "data/backup/$(date +"%Y_%m_%d").gsaid.db"
+python3 scripts/db_utils.py --targetdb "data/backup/$(date +"%Y_%m_%d").gsaid.db" >> debug/Migrate.log
 
 #Delete old database files
-rm -v !("data/backup/$(date +"%Y_%m_%d").gsaid.db")
+cd data/backup
+rm -v !("$(date +"%Y_%m_%d").gsaid.db")
