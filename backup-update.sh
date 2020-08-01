599d1dc7cdd09296a73a2c9335dee1a516c7d6d3
emmanuel <ewong347@uwo.ca>
2020-08-01 15:33:25 -0400

    Accounting for GISAID web platform changes, autobot switches to Covid frontpage

--- backup-update.sh
+++ backup-update.sh
@@ -12,3 +12,24 @@ python3 scripts/db_utils.py --targetdb "data/backup/$(date +"%Y_%m_%d").gsaid.db
 #Delete old database files
 cd data/backup
 rm -v !("$(date +"%Y_%m_%d").gsaid.db")
+
+# screen for non-human and low-coverage samples -> gisaid-filtered.fa
+python3 scripts/filtering.py
+
+# calculate TN93 distances
+tn93 -t 0.00005 -o data/gisaid.tn93.csv data/gisaid-filtered.fa
+
+# cluster genomes into variants -> variants.csv, variants.fa
+python3 scripts/variants.py
+
+# calculate TN93 distances for clusters and output as HyPhy matrix
+tn93 -o data/variants.tn93.txt -f hyphy data/variants.fa
+
+# convert HyPhy matrix format to CSV
+sed -i 's/[{}]//g' data/variants.tn93.txt
+
+# hierarchical clustering -> data/clusters.json
+Rscript scripts/hclust.R
+
+# run FastTree and TreeTime
+python3 scripts/treetime.py
