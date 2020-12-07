11eefede093d9f79b366e687315135b19a8fa247
ArtPoon <artpoon@gmail.com>
2020-12-06 21:50:52 -0500

    changed to MPI for clustering.py, #165

--- covizu/clustering.py
+++ covizu/clustering.py
@@ -1,5 +1,6 @@
 import random
 import json
+from mpi4py import MPI
 
 from covizu.utils import db_utils, seq_utils
 from covizu.utils.progress_utils import Callback
@@ -21,6 +22,9 @@ from covizu.utils.seq_utils import load_vcf, filter_problematic
 
 
 sys.setrecursionlimit(20000)  # fix for issue #127, default limit 1000
+comm = MPI.COMM_WORLD
+my_rank = comm.Get_rank()
+nprocs = comm.Get_size()
 
 
 def import_json(path, vcf_file, max_missing=600, callback=None):
@@ -82,10 +86,8 @@ def sample_with_replacement(population, k):
 
 def build_trees(records, nboot=100, binpath='rapidnj', threads=1, callback=None):
     """
-    Calculate symmetric differences of feature vectors.
-    Use nonparametric bootstrap sampling of the feature set union to convert symmetric
-    differences to distances by re-weighting features.
-    Reconstruct trees by neighbor-joining method.
+    Recode feature vectors with integer indices based on set union.
+    Pass results to bootstrap() to reconstruct trees by neighbor-joining method.
 
     :param records:  list, dict for each record
     :param nboot:  int, number of bootstrap samples
@@ -122,37 +124,58 @@ def build_trees(records, nboot=100, binpath='rapidnj', threads=1, callback=None)
     if callback:
         callback("Reconstructing trees ({} threads)".format(threads))
     if threads == 1:
-        trees = [bootstrap(union, indexed, binpath) for _ in range(nboot)]
+        # serial mode
+        trees = [bootstrap(union, indexed, binpath, callback=callback) for _ in range(nboot)]
     else:
+        """
         with Pool(threads) as pool:
             results = [pool.apply_async(bootstrap, [union, indexed, binpath]) for _ in range(nboot)]
             trees = [r.get() for r in results]
+        """
+        trees = []
+        for bn in range(nboot):
+            if bn % nprocs != my_rank:
+                continue
+            phy = bootstrap(union, indexed, binpath, callback=callback)
+            trees.append(phy)
+        comm.Barrier()
+        trees = comm.gather(trees, root=0)
 
     return trees, labels
 
 
-def bootstrap(union, indexed, binpath='rapidnj'):
+def bootstrap(union, indexed, binpath='rapidnj', callback=None):
+    """
+    Sample features from set union at random with replacement.  We use the
+    result to weight the symmetric differences when calculating pairwise
+    distances.  Pass the resulting distance matrix to RapidNJ to reconstruct
+    a tree.
+
+    :param union:  set, all observed genetic differences from reference (features)
+    :param indexed:  list, feature vectors encoded as integers
+    :param binpath:  str, path to RapidNJ binary executable
+    :param callback:  function, optional for progress monitoring
+    """
     sample = [int(len(union) * random.random()) for _ in range(len(union))]
     weights = dict([(y, sample.count(y)) for y in sample])
-
-    d = {}
     n = len(indexed)
-    for i in range(n - 1):
-        for j in range(i + 1, n):
-            sd = tuple(indexed[i] ^ indexed[j])
-            d[(i, j)] = sum(weights.get(y, 0) for y in sd)
 
-    # export to file
+    if callback:
+        callback("Writing distance matrix to temporary file")
+
     outfile = tempfile.NamedTemporaryFile('w', prefix="cvz_boot_")
     outfile.write('{0:>5}\n'.format(n))
     for i in range(n):
+        if callback and i % 100 == 0:
+            callback("  row {} of {}".format(i, n))
         outfile.write('{}'.format(i))
         for j in range(n):
             if i == j:
                 outfile.write(' {0:>2}'.format(0))
             else:
-                key = (i, j) if i < j else (j, i)
-                outfile.write(' {0:>2}'.format(d[key]))
+                sd = indexed[i] ^ indexed[j]  # symmetric difference
+                d = sum(weights.get(y, 0) for y in sd)
+                outfile.write(' {0:>2}'.format(d))
         outfile.write('\n')
     outfile.flush()
 
