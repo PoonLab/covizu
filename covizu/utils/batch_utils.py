69ab0aba72943a4bb6dc99817d204843c894a8e5
Art Poon <artpoon@gmail.com>
2021-05-27 15:11:46 -0400

    make utils/batch_utils.py build_timetree() always call earliest=True

--- covizu/utils/batch_utils.py
+++ covizu/utils/batch_utils.py
@@ -6,7 +6,7 @@ from covizu import clustering, treetime, beadplot
 def build_timetree(by_lineage, args, callback=None):
     """ Generate time-scaled tree of Pangolin lineages """
     fasta = treetime.retrieve_genomes(by_lineage, ref_file=args.ref, 
-                                      earliest=args.earliest)
+                                      earliest=True)
 
     if callback:
         callback("Reconstructing tree with {}".format(args.ft2bin))
