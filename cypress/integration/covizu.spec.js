23a5fc66ea1c8d6ac12d7041c2025a7b868e29f8
bonnielu <bonnielu2002@gmail.com>
2021-06-09 10:06:03 -0400

    front end tests for tooltips and search

--- cypress/integration/covizu.spec.js
+++ cypress/integration/covizu.spec.js
@@ -23,7 +23,7 @@ describe('CoVizu page', () => {
         cy.get('#splash').should('be.visible')
         cy.get('#splash-button').should('be.disabled')
     })
-    it('Splash button is enabled', () => {
+    it('Splash button is enabled after clusters.json loads', () => {
         cy.get('#splash-button', {timeout: 40000}).should('be.enabled')
         cy.get('#splash-button').click().should('not.be.visible')
     })
@@ -62,6 +62,24 @@ describe('Search Interface', () => {
         cy.get('#end-date').type('20210513')
         cy.get('#search-button').click({force:true})
         cy.contains('Invalid End Date (YYYY-MM-DD)')
+        cy.get('#end-date').clear()
+    })
+    it("Searching 'HMH' results in selection and expected point count", () => {
+        cy.get('#search-input').type('HMH')
+        cy.get('#search-button').click({force:true})
+        cy.get('.selectionH').should('exist')
+        cy.get('.SelectedCluster').should('exist')
+        cy.window().then((win) => {
+            cy.get('#search_stats').contains(`1 of ${win.bead_id_to_accession.length} points`)
+        })
+    })
+    it("Clicking the next button highlights the correct bead", () => {
+        cy.window().then((win) => {
+            cy.get('.selectionH').should('have.attr', 'bead', `${win.bead_id_to_accession[0]}`)
+            cy.get('#next_button').click()
+            cy.get('.selectionH').should('have.length', 1)
+            cy.get('.selectionH').should('have.attr', 'bead', `${win.bead_id_to_accession[1]}`)
+        })
     })
     it('Clear button resets search interface and cluster selection', () => {
         cy.get('#clear_button').click()
@@ -71,5 +89,31 @@ describe('Search Interface', () => {
         cy.get('#search_stats').contains('0 of 0 points')
         cy.get('.selectionH').should('not.exist')
         cy.get('.SelectedCluster').should('not.exist')
+    })
+})
+
+describe('Tooltips', () => {
+    it('Appear on hover over cluster', () => {
+        cy.get('rect:visible').first().trigger('mouseover')
+        cy.get('.tooltip').should('be.visible').should('have.length', 1)
+    })
+    it('Cluster tooltips contain relevant and correct information', () => {
+        cy.get('rect:visible').first().trigger('mouseover').invoke('attr','id').as('id')
+        cy.get('@id').then(id => {
+            var id_number = id.substring(3)
+            cy.window().then((win) => {
+                cy.get('.tooltip').contains(`Number of variants: ${win.tips[id_number]['varcount']}`)
+                cy.wrap(win.tips[id_number]['mutations']).each(($el) => {
+                    cy.get('.tooltip').contains($el)
+                })
+                cy.wrap(Object.keys(win.tips[id_number]['allregions'])).each(($el, index) => {
+                    cy.get('.tooltip').contains(`${$el}: ${Object.values(win.tips[id_number]['allregions'])[index]}`)
+                })        
+            })
+        })
+    })
+    it('Appear on hover over bead', () => {
+        cy.get('circle:visible').first().trigger('mouseover')
+        cy.get('.tooltip').should('be.visible').should('have.length', 1)
     })
 })
