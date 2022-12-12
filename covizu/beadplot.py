d88624f786a1f32677370c996346d76c97a1663e
GopiGugan <ggugan@uwo.ca>
2022-12-12 09:22:27 -0500

    Temporarily undoing changes related to #58

--- covizu/beadplot.py
+++ covizu/beadplot.py
@@ -134,7 +134,7 @@ def serialize_tree(tree):
     """
     obj = {'nodes': {}, 'edges': []}
     variant_d = {}
-    map_nodes= {}
+    # map_nodes= {}
     parents = get_parents(tree)
 
     us_count = 0  # number of unsampled variants
@@ -146,8 +146,8 @@ def serialize_tree(tree):
             variant = intermed[0][1]  # use accession of earliest sample to ID variant
 
             # Map the nodes to length and ascending dates
-            first_date = intermed[0][0]
-            last_date = intermed[len(intermed) - 1][0]
+            # first_date = intermed[0][0]
+            # last_date = intermed[len(intermed) - 1][0]
 
             # populate list with samples
             obj['nodes'].update({variant: intermed})
@@ -161,42 +161,46 @@ def serialize_tree(tree):
         if node is tree.root:
             continue  # no edge
 
-        if not node.labels:
-            # Set first date to first date of parent; last date to the earliest first date of a descendant
-            id = node
-            while parents[id] is not tree.root and not parents[id].labels:
-                id = parents[id]
+        # if not node.labels:
+        #     # Set first date to first date of parent; last date to the earliest first date of a descendant
+        #     id = node
+        #     while parents[id] is not tree.root and not parents[id].labels:
+        #         id = parents[id]
 
-            if parents[id] is tree.root and len(obj['nodes'][variant_d[parents[id]]]) == 0:
-                continue
+        #     if parents[id] is tree.root and len(obj['nodes'][variant_d[parents[id]]]) == 0:
+        #         continue
 
-            first_date = obj['nodes'][variant_d[parents[id]]][0][0]
-            last_date = datetime.today().strftime('%Y-%m-%d')
-            for child in node:
-                if(child.labels):
-                    intermed = [label.split('|')[::-1] for label in child.labels]
-                    intermed.sort()
-                    if (intermed[0][0] < last_date):
-                        last_date = intermed[0][0]
+        #     first_date = obj['nodes'][variant_d[parents[id]]][0][0]
+        #     last_date = datetime.today().strftime('%Y-%m-%d')
+        #     for child in node:
+        #         if(child.labels):
+        #             intermed = [label.split('|')[::-1] for label in child.labels]
+        #             intermed.sort()
+        #             if (intermed[0][0] < last_date):
+        #                 last_date = intermed[0][0]
 
         parent = parents[node]
-        date_diff = datetime.strptime(last_date, "%Y-%m-%d") - datetime.strptime(first_date, "%Y-%m-%d")
+        
+        # parent ID, child ID, branch length, node support
+        obj['edges'].append([variant_d[parent], variant, round(node.branch_length, 2),
+                                node.confidence])
+    #     date_diff = datetime.strptime(last_date, "%Y-%m-%d") - datetime.strptime(first_date, "%Y-%m-%d")
 
-        # date width, first date, parent ID, child ID, branch length, node support
-        if(variant_d[parent] in map_nodes):
-            map_nodes[variant_d[parent]].append([date_diff, first_date, variant_d[parent], variant, round(node.branch_length, 2),
-                             node.confidence])
-        else:
-            map_nodes[variant_d[parent]] = [[date_diff, first_date, variant_d[parent], variant, round(node.branch_length, 2),
-                             node.confidence]]
+    #     # date width, first date, parent ID, child ID, branch length, node support
+    #     if(variant_d[parent] in map_nodes):
+    #         map_nodes[variant_d[parent]].append([date_diff, first_date, variant_d[parent], variant, round(node.branch_length, 2),
+    #                          node.confidence])
+    #     else:
+    #         map_nodes[variant_d[parent]] = [[date_diff, first_date, variant_d[parent], variant, round(node.branch_length, 2),
+    #                          node.confidence]]
 
 
-    for parent, child in map_nodes.items():
-        child.sort(key=lambda x: (x[0], x[1]))
+    # for parent, child in map_nodes.items():
+    #     child.sort(key=lambda x: (x[0], x[1]))
 
-    for children in map_nodes.values():
-        for child in children:
-            obj['edges'].append(child[2:6])
+    # for children in map_nodes.values():
+    #     for child in children:
+    #         obj['edges'].append(child[2:6])
 
     return obj
 
