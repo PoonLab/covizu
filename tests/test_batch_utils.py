7d07605433b35f0779ed8d7dd308dac115d48f35
GopiGugan <ggugan@uwo.ca>
2023-02-23 18:18:01 -0500

    Update Unit Test for #453

--- tests/test_batch_utils.py
+++ tests/test_batch_utils.py
@@ -8,48 +8,48 @@ from covizu.utils.batch_utils import beadplot_serial, import_labels, make_beadpl
 class TestBeadplotSerial(unittest.TestCase):
     def setUp(self):
         self.expected = {
-            'lineage' : 'B.1.1.171',
-            'nodes' : {
-                'EPI_ISL_465679' :
-                [[
-                    '2020-03-27',
-                    'EPI_ISL_465679',
-                    'hCoV-19/Canada/Qc-L00240569/2020'
-                ]]
+            'lineage': 'B.1.1.171',
+            'nodes': {
+                'EPI_ISL_465679': [['2020-03-27', 'EPI_ISL_465679', 'hCoV-19/Canada/Qc-L00240569/2020']]
             },
-            'edges' : []
+            'edges': [],
+            'sampled_variants': 1
         }
 
     def testBeadplotSerial(self):
         # Lineage with only one variant
 
         lineage = 'B.1.1.171'
-        features = [
-            {
-                'covv_virus_name' : 'hCoV-19/Canada/Qc-L00240569/2020',
-                'covv_accession_id' : 'EPI_ISL_465679',
-                'covv_collection_date': '2020-03-27',
-                'covv_lineage' : 'B.1.1.171',
-                'diffs' : [
-                    tuple(['~', 240, 'T']),
-                    tuple(['~', 1436, 'T']),
-                    tuple(['~', 3036, 'T']),
-                    tuple(['~', 3713, 'T']),
-                    tuple(['~', 5883, 'T']),
-                    tuple(['~', 14407, 'T']),
-                    tuple(['~', 20543, 'T']),
-                    tuple(['~', 23402, 'G']),
-                    tuple(['~', 28880, 'A']),
-                    tuple(['~', 28881, 'A']),
-                    tuple(['~', 28882, 'C'])
-                ],
-                'missing' : [
-                    tuple([0, 6]),
-                    tuple([5203, 5222]),
-                    tuple([29844, 29903])
-                ]
-            }
-        ]
+        features = {
+            '~|240|T,~|1436|T,~|3036|T,~|3713|T,~|5883|T,~|14407|T,~|20543|T,~|23402|G,~|28880|A,~|28881|A,~|28882|C': [
+                {
+                    'covv_virus_name': 'hCoV-19/Canada/Qc-L00240569/2020',
+                    'covv_accession_id': 'EPI_ISL_465679',
+                    'covv_collection_date': '2020-03-27',
+                    'covv_lineage': 'B.1.1.171',
+                    'covv_location': 'North America / Canada / London',
+                    'diffs' : [
+                        tuple(['~', 240, 'T']),
+                        tuple(['~', 1436, 'T']),
+                        tuple(['~', 3036, 'T']),
+                        tuple(['~', 3713, 'T']),
+                        tuple(['~', 5883, 'T']),
+                        tuple(['~', 14407, 'T']),
+                        tuple(['~', 20543, 'T']),
+                        tuple(['~', 23402, 'G']),
+                        tuple(['~', 28880, 'A']),
+                        tuple(['~', 28881, 'A']),
+                        tuple(['~', 28882, 'C'])
+                    ],
+                    'missing' : [
+                        tuple([0, 6]),
+                        tuple([5203, 5222]),
+                        tuple([29844, 29903])
+                    ]
+                }
+            ]
+        }
+
         args = Namespace(boot_cuttoff=0.5)
         result = beadplot_serial(lineage, features, args)
         self.assertEqual(self.expected, result)
